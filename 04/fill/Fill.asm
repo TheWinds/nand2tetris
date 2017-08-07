48dcb6a4ac9c96972e38bae0bcbb0ffff432a44e
thewinds <wind_ns@163.com>
2017-08-07 22:46:20 +0800

    完成按键控制屏幕颜色填充

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,43 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@color
+M=0
+
+(MAINLOOP)
+@color
+M=0
+@SCREEN
+D=A
+@i
+M=D
+
+@KBD
+D=M
+@FILLSCREEN
+D;JEQ
+@SETBLACK
+0;JMP
+
+(SETBLACK)
+@color
+M=-1
+@FILLSCREEN
+0;JMP
+
+(FILLSCREEN)
+@i
+D=M
+@KBD
+D=D-A
+@MAINLOOP
+D;JEQ
+@color
+D=M
+@i
+A=M
+M=D
+@i
+M=M+1
+@FILLSCREEN
+0;JMP
