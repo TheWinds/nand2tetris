6f07bf9b56896ce0aea896657db09bfb4b25fe86
thewinds <wind_ns@163.com>
2017-08-07 21:46:40 +0800

    完成乘法器

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,45 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// init variables
+@R0
+D=M
+@base
+M=D
+@R1
+D=M
+@n
+M=D
+@i
+M=1
+@result
+M=0
+@R2
+M=0
+// mult
+(LOOP)
+@i
+D=M
+@n
+D=D-M
+@OVER
+D;JGT
+@base
+D=M
+@result
+D=D+M
+M=D
+@i
+M=M+1
+@LOOP
+0;JMP
+(OVER)
+@result
+D=M
+@R2
+M=D
+@END
+0;JMP
+(END)
+@END
+0;JMP
